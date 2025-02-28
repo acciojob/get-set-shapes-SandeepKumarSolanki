27d0a4ea1bba63edc95e0bf0e2bf96722ca69208
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-03-01 00:30:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -43,3 +43,17 @@ class Square extends Rectangle {
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
+
+// Creating a Rectangle instance
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.getWidth); // Output: 5
+console.log(rectangle.getHeight); // Output: 10
+console.log(rectangle.getArea()); // Output: 50
+
+// Creating a Square instance
+const square = new Square(7);
+console.log(square.width); // Output: 7
+console.log(square.height); // Output: 7
+console.log(square.getArea()); // Output: 49
+console.log(square.getPerimeter()); // Output: 28
+
