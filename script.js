2d7977849e0d4319892207af016842c36ff615fc
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-03-07 09:57:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,40 @@
-//complete this code
 class Rectangle {
-	constructor(width , height){
-		this.width = width;
-		this.height = height;
-	}
-	
-	setWidth(width){
-		this.width = width;
-	}
-	setHeight(height){
-		this.height = height;
-	}
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
 
-	
-	getWidth(){
-		return this.width;
-	}
-	getHeight(height){
-		return this.height;
-	}
+  setWidth(width) {
+    this.width = width;
+  }
 
-	
-	getArea(){
-		return (getWidth()*getHeight());
-	}
+  setHeight(height) {
+    this.height = height;
+  }
+
+  getWidth() {
+    return this.width;
+  }
+
+  getHeight() {
+    return this.height;
+  }
+
+  getArea() {
+    return this.width * this.height;  // Fix: Use this.width and this.height
+  }
 }
 
 class Square extends Rectangle {
-	constructor(side){
-		super(side , side);
-		// this.side = side;
-	}
+  constructor(side) {
+    super(side, side);  // Initialize both width and height with 'side'
+  }
 
-	getPerimeter(){
-		return 4 * this.side;
-	}
-	
+  getPerimeter() {
+    return 4 * this.width;  // Fix: Square has equal width and height, use this.width (or this.height)
+  }
 }
-
-
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
-
-/
